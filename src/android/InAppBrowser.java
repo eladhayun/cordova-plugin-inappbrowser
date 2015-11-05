4d45d991b0dc1ec21b8e9866593a850ac9338b69
Elad Hayun <elad.hayun@gmail.com>
2015-11-05 23:50:32 +0200

    Now using Uri.encode

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -344,20 +344,7 @@ public class InAppBrowser extends CordovaPlugin {
           if ("tel".equals(uri.getScheme())) {
               intent = new Intent(Intent.ACTION_CALL);
 
-              String number = new String(url.trim()
-                      .replace("tel:", "")
-                      .replace(" ", "%20").replace("&", "%26")
-                      .replace(",", "%2c").replace("(", "%28").replace(")", "%29")
-                      .replace("!", "%21").replace("=", "%3D").replace("<", "%3C")
-                      .replace(">", "%3E").replace("#", "%23").replace("$", "%24")
-                      .replace("'", "%27").replace("*", "%2A").replace("-", "%2D")
-                      .replace(".", "%2E").replace("/", "%2F").replace(":", "%3A")
-                      .replace(";", "%3B").replace("?", "%3F").replace("@", "%40")
-                      .replace("[", "%5B").replace("\\", "%5C").replace("]", "%5D")
-                      .replace("_", "%5F").replace("`", "%60").replace("{", "%7B")
-                      .replace("|", "%7C").replace("}", "%7D"));
-
-              intent.setData(Uri.parse("tel:" + number));
+              intent.setData(Uri.parse("tel:" + Uri.encode(url.trim().replace("tel:", ""))));
           }
 
           else {
